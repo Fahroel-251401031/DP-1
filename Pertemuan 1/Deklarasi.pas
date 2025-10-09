196914791b1b6e720f51dfaa3b9060163efef546
Fahroel-251401031 <rullokz0511@gmail.com>
2025-10-09 15:03:14 +0700

    Pertemuan 1: Pengenalan Pemrogrmaan & Pascal

---
+++ Pertemuan 1/Deklarasi.pas
@@ -0,0 +1,24 @@
+program pengenalan;
+uses crt;
+
+var // variabel --> simpan nilai yang berubah ubah
+    nama: string:
+    usia: integer;
+(konstanta = 'salam kenal')
+const
+    salam = 'salam kenal !';
+
+begin
+    clrscr;
+
+    writeln('salam')
+
+    write('masukkan nama : ');
+    readln(nama)
+    write('masukkan usia : ');
+    readln(usia)
+
+    writeln(nama)
+    writeln(usia)
+
+end.
