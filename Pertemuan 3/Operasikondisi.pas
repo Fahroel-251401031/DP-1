8bbc8753c253fd5c92b41658477e94e80a8f283a
Fahroel-251401031 <rullokz0511@gmail.com>
2025-10-09 15:05:09 +0700

    Pertemuan 3: Operasi Kondisi

---
+++ Pertemuan 3/Operasikondisi.pas
@@ -0,0 +1,34 @@
+program Operasi;
+uses crt;
+ var
+    nilai: integer;
+    hasil: chat;
+
+begin
+    clsrc;
+    write('masukkan nilai anda');
+    readln(nilai);
+    
+    //statement if
+    //if(handist) then
+    //begin
+    //statement
+    //end.
+    
+    //satu.khasus
+    //if (nilai > 80) then
+    //begin
+        //writeln('kamu lulus !'):
+    //end;
+    
+    //dua,Khasus
+    if (nilai > 80) then
+    begin
+        writeln('kamu lulus !');
+        end
+        else
+        begin
+            writeln('kamu tidak lulus!');
+        end;
+
+end.
